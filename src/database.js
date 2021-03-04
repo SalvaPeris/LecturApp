66dc4fcd9cbe820283665fc309528e8bc2e33d70
salvetaVLC <79948536+salvetaVLC@users.noreply.github.com>
2021-03-04 14:10:36 +0100

    Update database.js

--- src/database.js
+++ src/database.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
  .then(db => console.log('DB is connected'))
  .catch(err => console.error(err));*/
 
-mongoose.connect("mongodb://lecturadb:yHMJstzYOZg9dx9laDvhxm3yQTgQO1Ko204UrMk9OWuddggMMss1J8sQfLUmOh4ZWGZwW4KlTncdSbE64IGgIw==@lecturadb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@lecturadb@", {
+mongoose.connect("mongodb+srv://salva:salvadb@cluster0.nkpxr.mongodb.net/lecturadb?retryWrites=true&w=majority", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
