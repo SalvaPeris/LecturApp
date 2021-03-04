0e733b71e24f1b0c3e38f4b05b944c5f42dc40c6
salvetaVLC <79948536+salvetaVLC@users.noreply.github.com>
2021-03-04 12:18:03 +0100

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo/notes-db-app', {
+/*mongoose.connect('mongodb://mongo/notes-db-app', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+ .then(db => console.log('DB is connected'))
+ .catch(err => console.error(err));*/
+
+mongoose.connect("mongodb://lecturadb:yHMJstzYOZg9dx9laDvhxm3yQTgQO1Ko204UrMk9OWuddggMMss1J8sQfLUmOh4ZWGZwW4KlTncdSbE64IGgIw==@lecturadb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@lecturadb@", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
